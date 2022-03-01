6326e9b7857440de3f4df646c729ade05b42c7ad
KingMabuza <amal@amallevi.com>
2022-03-01 15:44:25 +0200

    first commit

---
+++ resources/views/english.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>GBC</title>
+</head>
+<body>
+  <div class="navbar-fixed">
+    <nav class="black">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo black-text"><img src="images/logo.png" height="70"></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services</a></li>
+        </ul>
+        </div>
+    </nav>
+  </div>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="#about">About</a></li>
+    <li><a href="#services">Services</a></li>
+  </ul>
+
+  <div id="banner">
+      <div class="container">
+        <h1 class="white-text">YOUR PARTNER OF CHOICE</h1>
+
+      </div>
+  </div>
+
+  <div id="about" class="section scrollspy">
+    <div class="center container">
+        <h3 class="green-text text-darken-4">Who We Are</h3>
+        <p>G.B.C.-SARL a highly diversified and broad organization which was established to penetrate, dominate and influence in
+        various functions of the business. Our products and services are dynamic and modern, which has been proved by the
+        experience which is held under our spell; however, G.B.C.-SARL is well respected for its effectiveness of new
+        development reliable business solutions and latest advanced technology. In other words, we move with times and trends.</p>
+
+        <p>We consider any project or task, no matter how big or small as a top priority and it is of vital importance to us to
+        ensure that our client receive great results and value for their money whilst getting peace of mind. In the provision of
+        our services, we have studied and understood the changes faced by the government and corporate companies which is the
+        bedrock of the provision of our services. We maintain business relationships with all our clients including government
+        departments, municipalities, private companies, public companies, close corporation as well as individuals. We emphasize
+        the use of our Client's Relationship.</p>
+    </div>
+
+  <div class="row container">
+    <div class="col s12 m12">
+      <div class="card white">
+        <div class="card-content">
+          <span class="card-title center green-text text-darken-4"><b>Mission</b></span>
+          <div class="card-action"></div>
+          <p>to serve our Customers / Client with the best products and to provide tailor made superior services that each client's
+        unique demand and satisfy that demand to the best of our ability at competitive prices. Part of our plan is to solve
+        challenges that other small business encounter daily and to provide them with effective business solution. We aim at
+        contributing towards economic growth, social stability and creating employment within the communities. We are committed
+        to offer reliable cost-effective customer orientated security services by selecting best security guards, train and
+        retain them to deliver quality service to our customers.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m12">
+        <div class="card white">
+            <div class="card-content">
+                <span class="card-title center green-text text-darken-4"><b>Values</b></span>
+                <div class="card-action"></div>
+                <p>Excellence orientated, optimistic management, total commitment reliability, Diligence, well experienced professionals,
+                accountability and responsibility, Accuracy and attention to details are the features of everything we do. We believe in
+                the following values: CEC • Treat all clients with dignity and respect • Honesty and Loyalty • Professional Conduct •
+                Quality Customer Services • Uphold PSIRA requirements and National Legislations</p>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  </div>
+
+  <div id="services" class="section scrollspy">
+    <div id="services-intro">
+        <h2 class="green-text text-darken-4">SERVICES</h2>
+    </div>
+  </div>
+  <div id="services-content">
+  <p class="container">We offer a variety of services which are carried out by well-trained, equipped and motivated
+    personnel. They are
+    monitored on hourly basis by line supervisors. Our services are as follows:</p>
+    <br/>
+    <ol class="container">
+        <li>Guarding (Armed and Unarmed security officers)</li>
+        <li>Private investigations</li>
+        <li>V.I.P Protection, Close protection or special services</li>
+        <li>Security Officer (Dog handler)</li>
+        <li>Access control and other related services</li>
+        <li>Retail Services</li>
+        <li>Armed Reaction/Response</li>
+        <li>CCTV Surveillance</li>
+        <li>Mining Security</li>
+        <li>National Key Point</li>
+        <li>Banking security</li>
+        <li>Events</li>
+    </ol>
+    <br/>
+    <p class="container">As front-line representatives of your company, our security guards wear a high-quality uniform and take pride in their
+    appearance and we can guarantee above average service. As part of our recruitment process we check the backgrounds of
+    all our potential security staff, many come to us from the armed forces and we particularly welcome those with first aid
+    skills or other relevant experience.</p>
+
+    <div class="container">
+        <a href="/secprofile.pdf" download>
+            <p class="green-text text-darken-4"><b>English Brochure</b></p>
+        </a>
+
+        <a href="/frencgsc.pdf" download>
+            <p class="green-text text-darken-4"><b>French Brochure</b></p>
+        </a>
+    </div>
+  </div>
+
+  <footer class="page-footer black">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <img src="/images/logo.png" height="120"/>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="green-text text-darken-4" href="#about">About Us</a></li>
+                    <li><a class="green-text text-darken-4" href="#services">Services</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © Copyright GCB
+        </div>
+    </div>
+</footer>
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+      M.AutoInit();
+  </script>
+</body>
+</html>
---
+++ resources/views/french.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>GBC</title>
+</head>
+<body>
+  <div class="navbar-fixed">
+    <nav class="black">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo black-text"><img src="images/logo.png" height="70"></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#about">A propos</a></li>
+            <li><a href="#services">Services</a></li>
+        </ul>
+        </div>
+    </nav>
+  </div>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="#about">A propos</a></li>
+    <li><a href="#services">Services</a></li>
+  </ul>
+
+  <div id="banner">
+      <div class="container">
+        <h1 class="white-text">Votre Partenaire de choix</h1>
+
+      </div>
+  </div>
+
+  <div id="about" class="section scrollspy">
+    <div class="center container">
+        <h3 class="green-text text-darken-4">Qui Nous Sommes</h3>
+        <p>G.B.C.-SARL est une organisation très diversifiée et étendue qui a été créée pour pénétrer, dominer et influencer
+        diverses fonctions de l'entreprise. Nos produits et services sont dynamiques et modernes, ce qui a été prouvé par
+        l'expérience qui s'est déroulée sous notre charme. Cependant, G.B.C-SARL est bien respectée pour l'efficacité de ses
+        nouvelles solutions de développement fiables et des technologies de pointe. En d'autres termes, nous évoluons avec les
+        époques et les tendances.</p>
+
+        <p>Nous considérons tous les projets et toutes les tâches, qu'ils soient grands ou petits, comme une priorité absolue et il
+        est d'une importance vitale que nous veillions à ce que nos clients obtiennent d'excellents résultats et en aient pour
+        leur argent, tout en ayant l'esprit tranquille. Dans la prestation de nos services, nous avons étudié et compris les
+        changements auxquels le gouvernement et les entreprises sont confrontés, ce qui constitue le fondement de la prestation
+        de nos services. Nous entretenons des relations commerciales avec tous nos clients, y compris les ministères, les
+        municipalités, les sociétés privées, les sociétés ouvertes, les sociétés proches et les particuliers. Nous soulignons
+        l'utilisation de la relation client CRC.</p>
+    </div>
+
+  <div class="row container">
+    <div class="col s12 m12">
+      <div class="card white">
+        <div class="card-content">
+          <span class="card-title center green-text text-darken-4"><b>Mission</b></span>
+          <div class="card-action"></div>
+          <p>La mission est de servir nos clients / clients avec les meilleurs produits et de fournir des services de qualité
+        supérieure sur mesure qui répondent à la demande unique de chaque client et qui satisfont cette demande au meilleur de
+        notre capacité à des prix compétitifs. Une partie de notre plan consiste à résoudre les problèmes rencontrés
+        quotidiennement par d'autres petites entreprises et à leur fournir une solution efficace. Notre objectif est de
+        contribuer à la croissance économique, à la stabilité sociale et à la création d'emplois au sein des communautés. Nous
+        nous engageons à offrir des services de sécurité fiables, rentables et axés sur le client en sélectionnant les meilleurs
+        agents de sécurité, en les formant et en les conservant pour fournir un service de qualité à nos clients.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m12">
+        <div class="card white">
+            <div class="card-content">
+                <span class="card-title center green-text text-darken-4"><b>Values</b></span>
+                <div class="card-action"></div>
+                <p>L'excellence, la gestion optimiste, l'engagement total, la fiabilité, la diligence des professionnels bien expérimentés,
+                la responsabilité et la précision et le souci du détail sont les caractéristiques de tout ce que nous faisons. Nous
+                croyons aux valeurs suivantes: • Traiter tous les clients avec dignité et respect. • Honnêteté et loyauté. • Conduite
+                professionnelle. • Service client de qualité. • Respecter les exigences de la PSIRA et les législations nationales.</p>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  </div>
+
+  <div id="services" class="section scrollspy">
+    <div id="services-intro">
+        <h2 class="green-text text-darken-4">SERVICES</h2>
+    </div>
+  </div>
+  <div id="services-content">
+  <p class="container">Nous offrons une variété de services qui sont effectués par du personnel bien formé, équipé et motivé. Ils sont
+surveillés toutes les heures par les supérieurs hiérarchiques. Nos services sont les suivants:</p>
+    <br/>
+    <ol class="container">
+        <li>Gardiennage (officiers de sécurité armés et non armés)</li>
+        <li>Enquêtes privées</li>
+        <li>V.I.P Protection, Protection rapprochée ou services spéciaux</li>
+        <li>Agent de sécurité (maître chien)</li>
+        <li>Contrôle d'accès et autres services connexes</li>
+        <li>Services de vente au detail</li>
+        <li>Réaction armée / Réponse</li>
+        <li>Surveillance CCTV</li>
+        <li>Sécurité manière</li>
+        <li>Point clé national</li>
+        <li>Sécurité bancaire</li>
+        <li>Événements</li>
+    </ol>
+    <br/>
+    <p class="container">As front-line representatives of your company, our security guards wear a high-quality uniform and take pride in their
+    appearance and we can guarantee above average service. As part of our recruitment process we check the backgrounds of
+    all our potential security staff, many come to us from the armed forces and we particularly welcome those with first aid
+    skills or other relevant experience.</p>
+
+    <div class="container">
+        <a href="/secprofile.pdf" download>
+            <p class="green-text text-darken-4"><b>English Brochure</b></p>
+        </a>
+
+        <a href="/frencgsc.pdf" download>
+            <p class="green-text text-darken-4"><b>French Brochure</b></p>
+        </a>
+    </div>
+  </div>
+
+  <footer class="page-footer black">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <img src="/images/logo.png" height="120"/>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="green-text text-darken-4" href="#about">About Us</a></li>
+                    <li><a class="green-text text-darken-4" href="#services">Services</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © Copyright GCB
+        </div>
+    </div>
+</footer>
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+      M.AutoInit();
+  </script>
+</body>
+</html>
